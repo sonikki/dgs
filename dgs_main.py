39e297925b8fd72c95ba0affc7f7ff5e76dde2dc
sonikki <esko.perkko@gmail.com>
2023-10-17 23:43:24 +0300

    FLASK framework + html and js

--- dgs_main.py
+++ dgs_main.py
@@ -1,46 +1,65 @@
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from tabulate import tabulate
 
-# Lue tiedosto Pandas DataFrameen
+app = Flask(__name__)
+
+# Lue CSV-tiedosto DataFrameen
 df = pd.read_csv("scorecards.csv")
-
-# Luo lista eri ratojen nimistä
 ratat = df['CourseName'].unique()
-
-# Tulosta käyttäjälle saatavilla olevat radat
-print("Saatavilla olevat radat:")
-for i, rata in enumerate(ratat, start=1):
-    print(f"{i}. {rata}")
-
-# Kysy käyttäjältä valittavaa radan numeroa
-valittu_rata_numero = int(input("Valitse radan numero: ")) - 1
-
-# Valitse haluttu rata
-valittu_rata = ratat[valittu_rata_numero]
-rata_data = df[df['CourseName'] == valittu_rata]
-
-# Lajittele suoritukset +/- sarakkeen mukaan ja ota top 10
-top_10 = rata_data.sort_values(by='+/-').head(10)
-
-# Näytä kaikkien pelaajien top 10 tulokset konsolissa
-print("Kaikkien pelaajien top 10 tulokset:")
-print(tabulate(top_10[['PlayerName', 'CourseName', 'Kaikki', '+/-']], headers='keys', tablefmt='grid', showindex=False))
-
-# Luo lista pelaajien nimistä
 pelaajat = df['PlayerName'].unique()
 
-# Tulosta pelaajien nimet
-print("Saatavilla olevat pelaajat:")
-for i, pelaaja in enumerate(pelaajat, start=1):
-    print(f"{i}. {pelaaja}")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Kysy käyttäjältä pelaajan numeroa
-valittu_pelaaja_numero = int(input("Valitse pelaajan numero: ")) - 1
+@app.route('/get_top_scores', methods=['POST'])
+def get_top_scores():
+    data = request.get_json()
+    rata_index = int(data['rata_index'])
+    pelaaja_index = int(data['pelaaja_index'])
 
-# Valitse haluttu pelaaja
-valittu_pelaaja = pelaajat[valittu_pelaaja_numero]
+    valittu_rata = ratat[rata_index]
+    valittu_pelaaja = pelaajat[pelaaja_index]
 
-# Näytä valitun pelaajan top 10 tulokset konsolissa
-valitun_pelaajan_tulokset = rata_data[rata_data['PlayerName'] == valittu_pelaaja].sort_values(by='+/-').head(10)
-print(f"\n{valittu_pelaaja}'n top 10 tulokset:")
-print(tabulate(valitun_pelaajan_tulokset[['PlayerName', 'CourseName', 'Kaikki', '+/-']], headers='keys', tablefmt='grid', showindex=False))
+    rata_data = df[df['CourseName'] == valittu_rata]
+    top_10_koko_rata = tabulate(rata_data.sort_values(by='+/-').head(10)[['PlayerName', 'CourseName', 'Kaikki', '+/-']], headers='keys', tablefmt='html', showindex=False).strip()
+    valitun_pelaajan_tulokset = rata_data[rata_data['PlayerName'] == valittu_pelaaja].sort_values(by='+/-').head(10)
+    top_10_pelaaja = tabulate(valitun_pelaajan_tulokset[['PlayerName', 'CourseName', 'Kaikki', '+/-']], headers='keys', tablefmt='html', showindex=False).strip()
+
+    response_data = {
+        'top_10_koko_rata': top_10_koko_rata,
+        'top_10_pelaaja': top_10_pelaaja
+    }
+
+    return jsonify(response_data)
+
+@app.route('/hae_radat')
+def hae_radat():
+    return jsonify(ratat.tolist())
+
+@app.route('/hae_pelaajat')
+def hae_pelaajat():
+    return jsonify(pelaajat.tolist())
+
+@app.route('/hae_tulokset', methods=['POST'])
+def hae_tulokset():
+    data = request.get_json()
+    rata = data['rata']
+    pelaaja = data['pelaaja']
+
+    print(f'Saatu rata: {rata}')
+    print(f'Saatu pelaaja: {pelaaja}')
+
+    # Hae tulokset tietokannasta käyttäen rata- ja pelaajatietoja
+    # Esimerkki: Oletetaan, että sinulla on DataFrame 'df', ja haet tulokset sen perusteella
+    tulokset = df.loc[(df['CourseName'] == rata) & (df['PlayerName'] == pelaaja)]
+    print(f'Löydetty {len(tulokset)} tulosta')  # Tulosten määrä
+
+    # Muunna tulokset JSON-muotoon
+    tulokset_json = tulokset.to_dict(orient='records')
+
+    return jsonify(tulokset_json)
+
+if __name__ == '__main__':
+    app.run(debug=True)
